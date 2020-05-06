61ac3c369f9662def08746dde70ee755ffa670d5
nikolicsmilan <nikolicsmilan@gmail.com>
2020-05-06 17:13:55 +0200

    'w'

---
+++ nikolics-milan-website/build/precache-manifest.d079415b7df4a8845f14aee26aac6a51.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27533dd06587b8c9c96e5c6723bc5b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fbde3f77e06389ff1d3",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "251c1ad55b6e42cb6c41",
+    "url": "/static/css/main.1cf548c2.chunk.css"
+  },
+  {
+    "revision": "8fbde3f77e06389ff1d3",
+    "url": "/static/js/2.b607c34e.chunk.js"
+  },
+  {
+    "revision": "6a7903cba7c07c04b74cefcf79c6a3f5",
+    "url": "/static/js/2.b607c34e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251c1ad55b6e42cb6c41",
+    "url": "/static/js/main.e3588cea.chunk.js"
+  },
+  {
+    "revision": "a277459dddbcac43ed44",
+    "url": "/static/js/runtime-main.3f67dda9.js"
+  },
+  {
+    "revision": "986fe0a4e9e69a1d00f2d53e648672fc",
+    "url": "/static/media/Bemutatkozo_Mobil.986fe0a4.jpg"
+  },
+  {
+    "revision": "cb38f7cf56db68cfd235210a3bdc5755",
+    "url": "/static/media/Bevetel2.cb38f7cf.PNG"
+  },
+  {
+    "revision": "bd7e975983e2bcf699eafc76e8bc208e",
+    "url": "/static/media/Ceges_Mobil.bd7e9759.png"
+  },
+  {
+    "revision": "5e3dd14f6126055f4a6f9911959d1a90",
+    "url": "/static/media/EU_logo_Desktop.5e3dd14f.jpg"
+  },
+  {
+    "revision": "ebac684e5e7f1a136ab287963239df7b",
+    "url": "/static/media/NM_kep.ebac684e.jpg"
+  },
+  {
+    "revision": "2a95df8c2dcb2f7f6a2bdcab9bc702e1",
+    "url": "/static/media/Nepszeru2.2a95df8c.PNG"
+  },
+  {
+    "revision": "b7c2fcd8f251992cc0baf3c4352c990d",
+    "url": "/static/media/Ugyfel2.b7c2fcd8.PNG"
+  },
+  {
+    "revision": "72c64d0dc7d78242a53f0a39be54a895",
+    "url": "/static/media/Ugyfelszerzo_Mobil-min.72c64d0d.png"
+  },
+  {
+    "revision": "aa7faa1ab55e03ae8178f0fbd3811c6e",
+    "url": "/static/media/email-min.aa7faa1a.PNG"
+  },
+  {
+    "revision": "08f4289352d84b00e3df3ccceda87e29",
+    "url": "/static/media/info-gomb.08f42893.svg"
+  },
+  {
+    "revision": "a19b9f9670bfdfd1c8ed733259010f89",
+    "url": "/static/media/nikolicsmilan_logo_9.a19b9f96.png"
+  },
+  {
+    "revision": "43a4286049ef8bf791d4bbc03728aeae",
+    "url": "/static/media/osztonzo_szoveg.43a42860.PNG"
+  },
+  {
+    "revision": "586316b4af3c5d4ade3c765a1bc41371",
+    "url": "/static/media/pipa2.586316b4.svg"
+  },
+  {
+    "revision": "c32d6193534657d4b244400fb2a73b38",
+    "url": "/static/media/telefon-min.c32d6193.PNG"
+  },
+  {
+    "revision": "df2168602b598200ff52be63bcde26ae",
+    "url": "/static/media/thinking.df216860.PNG"
+  },
+  {
+    "revision": "56f4be01b5d030427970a58ee32c510b",
+    "url": "/static/media/xa.56f4be01.svg"
+  }
+]);
